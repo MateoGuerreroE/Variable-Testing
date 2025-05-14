488168b90f50b531be82e3aa38f42506d75cab03
Mateo Guerrero <mateof1223@gmail.com>
2025-05-13 20:25:24 -0500

    Upload Coursera Repo

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }); });
+
+// Configure Serilog for logging
+builder.Host.UseSerilog((context, config) => { config.WriteTo.Console(); });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
